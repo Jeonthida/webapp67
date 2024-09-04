11f3aa2e30c1eb7bc80cdf884e22d17db31a560f
Jeonthida <147261895+Jeonthida@users.noreply.github.com>
2024-09-04 11:53:15 +0700

    Delete exercise03.js

--- exercise03.js
+++
@@ -1,4 +0,0 @@
-// const digitize = n => [...'${n}'].map(i => parseInt(i));
-const digitize = n =>[...`${Math.abs(n)}`].map(i => parseInt(i));
-console.log(digitize(123));
-console.log(digitize(1230));
